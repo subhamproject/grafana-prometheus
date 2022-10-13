ca86a2b57a8a444a921b9300c6634b0fcc4cc161
subhammandal <subhammandal01@gmail.com>
2022-10-13 08:49:51 +0000

    Added grafana dashboard export script

---
+++ export_grafana_dashboard.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+
+:${1?Please Provide Grafana URL Full Path -> Usage: ./$0 https://localhost:3000 eyJrIjoiOWpqRXpONHo5UktBSlpTZ2ZP}
+:${2?Please Provide Grafana Token -> Usage: ./$0 https://localhost:3000 eyJrIjoiOWpqRXpONHo5UktBSlpTZ2ZP}
+
+
+
+FULLURL="$1"
+headers="Authorization: Bearer $2"
+in_path=dashboards_raw
+set -o nounset
+
+echo "Exporting Grafana dashboards from $FULLURL"
+mkdir -p $in_path
+for dash in $(curl -H "$headers" -s "$FULLURL/api/search?query=&" | jq -r '.[] | select(.type == "dash-db") | .uid'); do
+        dash_path="$in_path/$dash.json"
+        curl -H "$headers" -s "$FULLURL/api/dashboards/uid/$dash" | jq -r . > $dash_path
+        slug=$(cat $dash_path | jq -r '.meta.slug')
+        folder="$(cat $dash_path | jq -r '.meta.folderTitle')"
+        mkdir -p "$folder"
+        mv -fv $dash_path $folder/${dash}-${slug}.json
+done
+
+mv General Dashboard_Backup
+
+
+
+#bash export.sh http://localhost:3000 "eyJrIjoiOWpqRXpONHo5UktBSlpTZ2ZPSm1YWDU2eHQ2NnV6MmsiLCJuIjoiZmV0Y2giLCJpZCI6MX0="
+#https://gist.github.com/crisidev/bd52bdcc7f029be2f295
